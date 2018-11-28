15ef1b0836c1e2df3561a069721a4cf25b24b675
SFFan123 <jack.o.neill.fabi@t-online.de>
2018-11-28 10:30:26 +0100

    setting up test app

---
+++ TestApp/User.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PC_Verwaltung
+{
+    class User
+    {
+        public string username { get; private set;}
+        public string password { get; private set; }
+
+        public User(string username, string password)
+        {
+            this.username = username;
+            this.password = sha256(password);
+        }
+
+        private string sha256(string s)
+        {
+            var crypt = new System.Security.Cryptography.SHA256Managed();
+            var hash = new System.Text.StringBuilder();
+            byte[] crypto = crypt.ComputeHash(Encoding.UTF8.GetBytes(s));
+            foreach (byte theByte in crypto)
+            {
+                hash.Append(theByte.ToString("x2"));
+            }
+            return hash.ToString();
+        }
+
+    }
+}
