c1294bb3b4f70b82939fcb9ca612ceb37c5606f4
SFFan123 <jack.o.neill.fabi@t-online.de>
2018-12-07 11:55:34 +0100

    updated TestApp

--- TestApp/User.cs
+++ TestApp/User.cs
@@ -8,79 +8,82 @@ namespace TestApp
 {
     public class User
     {
-            public string name { get; private set; }
+        public string name { get; private set; }
 
-            public string surname { get; private set; }
+        public string surname { get; private set; }
+        
+        /// <summary>
+        /// Nicht veränderbar!
+        /// </summary>
+        public string username { get; }
 
-            public string username { get; private set; }
+        public string email { get; private set; }
 
-            public string email { get; private set; }
+        public string password { get; private set; }
 
-            public string password { get; private set; }
+        /// <summary>
+        /// [!Zukünftige Funktion!]
+        /// </summary>
+        public bool canEditUser { get; private set; }
 
-            /// <summary>
-            /// [!Zukünftige Funktion!]
-            /// </summary>
-            public bool canEditUser { get; private set; }
+        /// <summary>
+        /// [!Zukünftige Funktion!]
+        /// </summary>
+        public bool canEditRecords { get; private set; }
 
-            /// <summary>
-            /// [!Zukünftige Funktion!]
-            /// </summary>
-            public bool canEditRecords { get; private set; }
+        /// <summary>
+        /// [!Zukünftige Funktion!]
+        /// </summary>
+        public bool isAdmin { get; private set; }
 
-            /// <summary>
-            /// [!Zukünftige Funktion!]
-            /// </summary>
-            public bool isAdmin { get; private set; }
-
-            /* Alter Constructor 
-            public User(string username, string password)
+        /* Alter Constructor 
+        public User(string username, string password)
+        {
+            this.username = username;
+            this.password = sha256(password);
+        }
+        */
+        /// <summary>
+        /// Erstellt ein neues User Objekt.
+        /// </summary>
+        /// <param name="username">Der Username des Users</param>
+        /// <param name="password">Das Passwort (gehasht von der DB oder im Klartext von der Nutzereingabe)</param>
+        /// <param name="hashPassword">true falls das PW gehast werden muss, false falls das PW bereits gehasht ist.</param>
+        public User(string name, string surname, string username, string email, string password, Boolean hashPassword)
+        {
+            if(hashPassword)
             {
+                this.name = name;
+                this.surname = surname;
                 this.username = username;
+                this.email = email;
                 this.password = sha256(password);
             }
-            */
-            /// <summary>
-            /// Erstellt ein neues User Objekt.
-            /// </summary>
-            /// <param name="username">Der Username des Users</param>
-            /// <param name="password">Das Passwort (gehasht von der DB oder im Klartext von der Nutzereingabe)</param>
-            /// <param name="hashPassword">true falls das PW gehast werden muss, false falls das PW bereits gehasht ist.</param>
-            public User(string name, string surname, string username, string email, string password, Boolean hashPassword)
+            else
             {
-                if (hashPassword)
-                {
-                    this.name = name;
-                    this.surname = surname;
-                    this.username = username;
-                    this.email = email;
-                    this.password = sha256(password);
-                }
-                else
-                {
-                    this.name = name;
-                    this.surname = surname;
-                    this.username = username;
-                    this.email = email;
-                    this.password = password;
-                }
+                this.name = name;
+                this.surname = surname;
+                this.username = username;
+                this.email = email;
+                this.password = password;
             }
-            /// <summary>
-            /// Hasht das Passwort mit sha256.
-            /// </summary>
-            /// <param name="s">Das Passwort das Gehast werden soll im Klartext</param>
-            /// <returns></returns>
-            public static string sha256(string s)
-            {
-                var crypt = new System.Security.Cryptography.SHA256Managed();
-                var hash = new System.Text.StringBuilder();
-                byte[] crypto = crypt.ComputeHash(Encoding.UTF8.GetBytes(s));
-                foreach (byte theByte in crypto)
-                {
-                    hash.Append(theByte.ToString("x2"));
-                }
-                return hash.ToString();
-            }
-
         }
+        /// <summary>
+        /// Hasht das Passwort mit sha256.
+        /// </summary>
+        /// <param name="s">Das Passwort das Gehast werden soll im Klartext</param>
+        /// <returns></returns>
+        public static string sha256(string s)
+        {
+            var crypt = new System.Security.Cryptography.SHA256Managed();
+            var hash = new System.Text.StringBuilder();
+            byte[] crypto = crypt.ComputeHash(Encoding.UTF8.GetBytes(s));
+            foreach (byte theByte in crypto)
+            {
+                hash.Append(theByte.ToString("x2"));
+            }
+            return hash.ToString();
+        }
+        
+    }
 }
